0802d7e3adbf14e0c2a84e35015d779384653328
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2023-04-20 09:12:05 -0400

    Update calculaIMC.js
    Uso de operadores ternários.

--- back-end_nodejs/services/calculaIMC.js
+++ back-end_nodejs/services/calculaIMC.js
@@ -1,30 +1,12 @@
-const calculadoraIMC = (peso, altura) => {
-  let imc = peso / (altura * altura);
-  return imc.toFixed(2);
-};
+const calculadoraIMC = (peso, altura) => (peso / (altura * altura)).toFixed(2);
 
-const retornaStatusIMC = (imc) => {
-  let status;
+const retornaStatusIMC = (imc) => 
+  imc < 18.5 ? "Abaixo do peso" :
+  imc >= 18.5 && imc <= 24.8 ? "Peso normal" :
+  imc >= 24.9 && imc < 30 ? "Acima do peso" :
+  "Obeso";
 
-  if (imc < 18.5) {
-    status = "Abaixo do peso";
-  } else if (imc >= 18.5 && imc <= 24.8) {
-    status = "Peso normal";
-  } else if (imc >= 24.9 && imc < 30) {
-    status = "Acima do peso";
-  } else {
-    status = "Obeso";
-  }
-  return status;
-};
-
-const parameterValidation = (parameter) => {
-  if (isNaN(parameter)) {
-    return false;
-  } else {
-    return true;
-  }
-};
+const parameterValidation = (parameter) => isNaN(parameter) ? false : true;
 
 exports.parameterValidation = parameterValidation;
 exports.retornaStatusIMC = retornaStatusIMC;
